fe25bf093ec776c0680456a5c97ac2f6cd13593f
Ihor Kozlov <i.kozlov.tech@gmail.com>
2023-07-18 01:21:45 +0300

    Fix button, add "appBar"

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 import 'register_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -31,12 +32,32 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipPath(
+              clipper: WaveClipperOne(),
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xff673ab7), Color(0xff9c27b0)],
+                    stops: [0.1, 1],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+                height: 100,
+                //color: Colors.deepPurple,
+                child: Center(child: Text("Note",style: TextStyle(fontSize:32,
+                    color: Colors.white,
+                    letterSpacing: 12),
+                )),
+              ),
+            ),
+            SizedBox(height: 50,),
+            Center(
+              child: Padding(
                 padding: EdgeInsets.only(left: 65),
                 child: Image.asset('assets/img/log.png',
                     width: 200,
@@ -44,111 +65,111 @@ class _LoginPageState extends State<LoginPage> {
                     alignment: Alignment.center,
                   ),
               ),
+            ),
 
-              /*const Icon(
-                Icons.android,
-                size: 100,
-              ),*/
-              const SizedBox(
-                height: 25,
-              ),
-              const Text(
-                'WELCOME',
-                style: TextStyle(fontSize: 48),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                'Welcome Back!',
-                style: TextStyle(fontSize: 20),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    hintText: 'Email',
-                    fillColor: Colors.grey[200],
-                    filled: true,
+            /*const Icon(
+              Icons.android,
+              size: 100,
+            ),*/
+            const SizedBox(
+              height: 25,
+            ),
+            const Text(
+              'WELCOME',
+              style: TextStyle(fontSize: 48),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'Welcome Back!',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12.0),
                   ),
+                  hintText: 'Email',
+                  fillColor: Colors.grey[200],
+                  filled: true,
                 ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
 
-              //password field
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: TextField(
-                  obscureText: true,
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    hintText: 'Password',
-                    fillColor: Colors.grey[200],
-                    filled: true,
+            //password field
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: TextField(
+                obscureText: true,
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12.0),
                   ),
+                  hintText: 'Password',
+                  fillColor: Colors.grey[200],
+                  filled: true,
                 ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: GestureDetector(
-                  onTap: signIn,
-                  child: Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Sign In',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: GestureDetector(
+                onTap: signIn,
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
                       ),
                     ),
                   ),
                 ),
               ),
+            ),
 
-              const SizedBox(
-                height: 25,
-              ),
+            const SizedBox(
+              height: 25,
+            ),
 
-              //Register Now
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Not a member? '),
-                  GestureDetector(
-                    onTap: widget.showRegisterPage,
-                    child: const Text(
-                      'Register now',
-                      style: TextStyle(color: Colors.deepPurpleAccent),
-                    ),
+            //Register Now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Not a member? '),
+                GestureDetector(
+                  onTap: widget.showRegisterPage,
+                  child: const Text(
+                    'Register now',
+                    style: TextStyle(color: Colors.deepPurpleAccent),
                   ),
-                ],
-              )
-            ],
-          ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class RegisterPage extends StatefulWidget {
   final VoidCallback showLoginPage;
@@ -45,136 +46,154 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 65),
-                child: Image.asset('assets/img/log.png',
-                  width: 200,
-                  height: 200,
-                  alignment: Alignment.center,
-                ),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              const Text(
-                'REGISTER',
-                style: TextStyle(fontSize: 48),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                'Register below!',
-                style: TextStyle(fontSize: 20),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    hintText: 'Email',
-                    fillColor: Colors.grey[200],
-                    filled: true,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipPath(
+              clipper: WaveClipperOne(),
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xff673ab7), Color(0xff9c27b0)],
+                    stops: [0.1, 1],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
                   ),
                 ),
+                height: 100,
+                //color: Colors.deepPurple,
+                child: const Center(child: Text("Note",style: TextStyle(fontSize:32,
+                    color: Colors.white,
+                    letterSpacing: 12),
+                )),
               ),
-              const SizedBox(
-                height: 10,
+            ),
+            const SizedBox(height: 50,),
+            Padding(
+              padding: const EdgeInsets.only(left: 65),
+              child: Image.asset('assets/img/log.png',
+                width: 200,
+                height: 200,
+                alignment: Alignment.center,
               ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            const Text(
+              'REGISTER',
+              style: TextStyle(fontSize: 48),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'Register below!',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  hintText: 'Email',
+                  fillColor: Colors.grey[200],
+                  filled: true,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
 
-              //password field
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: TextField(
-                  obscureText: true,
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    hintText: 'Password',
-                    fillColor: Colors.grey[200],
-                    filled: true,
+            //password field
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: TextField(
+                obscureText: true,
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12.0),
                   ),
+                  hintText: 'Password',
+                  fillColor: Colors.grey[200],
+                  filled: true,
                 ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: TextField(
-                  obscureText: true,
-                  controller: _confirmPasswordController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    hintText: 'Confirm password',
-                    fillColor: Colors.grey[200],
-                    filled: true,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: TextField(
+                obscureText: true,
+                controller: _confirmPasswordController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12.0),
                   ),
+                  hintText: 'Confirm password',
+                  fillColor: Colors.grey[200],
+                  filled: true,
                 ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: GestureDetector(
-                  onTap: signUp,
-                  child: Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Sign In',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: GestureDetector(
+                onTap: signUp,
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
                       ),
                     ),
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 25,
-              ),
-              //Register Now
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('I am a member? '),
-                  GestureDetector(
-                    onTap: widget.showLoginPage,
-                    child: const Text(
-                      'Login In',
-                      style: TextStyle(color: Colors.deepPurpleAccent),
-                    ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            //Register Now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('I am a member? '),
+                GestureDetector(
+                  onTap: widget.showLoginPage,
+                  child: const Text(
+                    'Login In',
+                    style: TextStyle(color: Colors.deepPurpleAccent),
                   ),
-                ],
-              )
-            ],
-          ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
